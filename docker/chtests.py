5d4f1592f647b798ec6d6f9690f88180214132f2
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-08-07 10:07:43 -0500

    Adding docker testsuite

---
+++ docker/chtests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import os, sys, re
+
+for f in os.listdir("arrangements"):
+    for f2 in os.listdir("arrangements/"+f):
+        full = "arrangements/"+f+"/"+f2+"/test"
+        if not os.path.exists(full):
+            continue
+        try:
+            for t in os.listdir(full):
+                if not re.match(r'.*\.par$',t):
+                    continue
+                test = full+"/"+t
+                save = full+"/."+t+".sav"
+                if os.path.exists(save):
+                    fd = open(save,"r")
+                    contents = fd.read()
+                    fd.close()
+                else:
+                    fd = open(test,"r")
+                    contents = fd.read()
+                    fd.close()
+                    fd = open(save,"w")
+                    fd.write(contents)
+                    fd.close()
+                m = "T"
+                for a in sys.argv[1:]:
+                    g = re.match(r'^(\s*[^=\s]*)\s*=\s*(.*\S)',a)
+                    if g:
+                        pat = re.compile(r'(?mi)^'+g.group(1)+r'\s*=.*')
+                        if re.search(pat,contents):
+                            contents = re.sub(pat,a,contents)
+                            m="M"
+                        else:
+                            contents += '\n'+a
+                            m ="F"
+                fd = open(test,"w")
+                fd.write(contents)
+                fd.close()
+        except Exception as ex:
+            print(full,ex)
---
+++ docker/runtests2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python3 chtests.py 'carpet::use_psync=yes' 'carpet::psync_only=yes' 'carpet::psync_error=yes'
+
+# Test
+for nprocs in 1 2; do
+    ./simfactory/bin/sim create-run test$nprocs --walltime 1:00:00 --testsuite --procs $nprocs --num-threads 1 --ppn-used=$nprocs > pout-$nprocs.txt
+done
+
+python3 chtests.py 'carpet::use_psync=no' 'carpet::psync_only=no' 'carpet::psync_error=no'
+
+# Test
+for nprocs in 1 2; do
+    ./simfactory/bin/sim create-run ptest$nprocs --walltime 1:00:00 --testsuite --procs $nprocs --num-threads 1 --ppn-used=$nprocs > out-$nprocs.txt
+done
